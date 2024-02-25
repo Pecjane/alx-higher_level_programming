d52f5f3ea914d2fefab6fc47e80e407831f3f8f7
Pecjane <promisebarth@gmail.com>
2024-02-25 20:11:35 +0100

    another key

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,2 +1,15 @@
 #!/usr/bin/node
-console.log(parseInt(process.argv[2]) ? `My number: ${parseInt(process.argv[2])}` : 'Not a number');}
+
+/*
+ *  * prints My number: <first argument converted in integer>
+ *   * if the first argument can be converted to an integer
+ *    * else prints "Not a number".
+ *     */
+
+const arg = process.argv[2];
+
+if (isNaN(parseInt(arg))) {
+	  console.log('Not a number');
+} else {
+	  console.log('My number:', parseInt(arg));
+}
