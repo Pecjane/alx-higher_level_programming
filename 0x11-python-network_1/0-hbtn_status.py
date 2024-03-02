7c1cc99d4e492f3bf8f317dbf9b58f12992621f1
Pecjane <promisebarth@gmail.com>
2024-03-02 01:41:39 +0100

    status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Fetches https://alx-intranet.hbtn.io/status
+"""
+
+from urllib import request
+
+if __name__ == "__main__":
+        with request.urlopen('https://alx-intranet.hbtn.io/status') as url:
+                    data = url.read()
+                            print('Body response:')
+                                    print(f'\t- type: {type(data)}')
+                                            print(f'\t- content: {data}')
+                                                    print(f'\t- utf8 content: {data.decode("utf-8")}')
