4f700dbc092a99c076ac37a14a400bdb384102d4
Pecjane <promise.barth@gmail.com>
2024-03-26 18:26:56 -0700

    status code

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/**
+ * Writes a string to a file.
+ */
+
+const process = require('process');
+const fs = require('fs');
+
+fs.writeFile(process.argv[2], process.argv[3], (err) => {
+	if (err) {
+		console.log(err);
+	}
+}
+);
