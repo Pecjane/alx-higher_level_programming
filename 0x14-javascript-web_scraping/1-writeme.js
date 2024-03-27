f223969b4c1617d11beb885165fd9e4316b93d5e
Pecjane <promise.barth@gmail.com>
2024-03-26 19:03:03 -0700

    scrapping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
-/**
- * Writes a string to a file.
- */
-
-const process = require('process');
 const fs = require('fs');
+const process = require('process');
+const filePath = process?.argv[2];
+const stringToWrite = process?.argv[3];
 
-fs.writeFile(process.argv[2], process.argv[3], (err) => {
-	if (err) {
-		console.log(err);
-	}
-}
-);
+fs.writeFile(filePath, stringToWrite, 'utf-8', (error) => {
+  if (error) return console.log(error);
+});
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-	console.log(error || JSON.parse(body).title);
+const movieId = process?.argv[2];
+const requestURL = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(requestURL, (error, response) => {
+  if (error) return console.log(error);
+  console.log(JSON.parse(response.body));
 });
